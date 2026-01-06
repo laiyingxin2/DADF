6b3fc2c2e987b8e49eebfeab0174e0b35527e8ce
laiyingxin <100742127+laiyingxin2@users.noreply.github.com>
2026-01-06 12:00:54 +0800

    Modify run.sh for multi-GPU training with demo.yaml
    Updated the run script to use multiple GPUs and a new config file.

--- run.sh
+++ run.sh
@@ -1,16 +1,3 @@
-#!/bin/sh
-
-echo "开始测试......"
-#python3 -m torch.distributed.launch --master_port=12000 --nnodes 1 --nproc_per_node 1 train.py --config configs/Deepfakes.yaml
-# wait
-#python3 -m torch.distributed.launch --master_port=12000 --nnodes 1 --nproc_per_node 1 train.py --config configs/Face2Face.yaml
-# wait
-# # python3 -m torch.distributed.launch --master_port=12000 --nnodes 1 --nproc_per_node 1 train.py --config configs/FaceSwap.yaml
-# # wait
-#python3 -m torch.distributed.launch --master_port=12000 --nnodes 1 --nproc_per_node 1 train.py --config configs/NeuralTextures.yaml
-python -m torch.distributed.launch train.py --nnodes 1 --nproc_per_node 1 --config configs/NeuralTextures.yaml
-
-# # wait
-echo "结束测试......"
-
-#wait能等待前一个脚本执行完毕，再执行下一个条命令；#
+CUDA_VISIBLE_DEVICES=0,1,4,3 \
+python -m torch.distributed.launch --nnodes=1 --nproc_per_node=4 --master_port=29501 \
+train.py --config configs/demo.yaml
